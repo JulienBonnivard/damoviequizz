d7aa48540bbde9a902170bfb5444d2e9fd56b73e
Julien MobileGlobe <bonnivard@gmail.com>
2016-01-22 15:55:51 +0100

    Initial Commit

---
+++ DaMovieQuizz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DaMovieQuizz
+//
+//  Created by Julien MobileGlobe on 22/01/2016.
+//  Copyright © 2016 JB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DaMovieQuizz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DaMovieQuizz
+//
+//  Created by Julien MobileGlobe on 22/01/2016.
+//  Copyright © 2016 JB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
